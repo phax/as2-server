fabd8a4c592369b1c37e66b14950bef2219d21f9
Philip Helger <philip@helger.com>
2015-10-20 16:07:59 +0200

    Added test for issue #12

---
+++ src/test/java/com/helger/as2/test/MainStreamToServer.java
@@ -0,0 +1,38 @@
+package com.helger.as2.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import com.helger.commons.io.file.FileHelper;
+import com.helger.commons.io.stream.StreamHelper;
+
+/**
+ * Small main application that writes a fully fledged AS2 message via a socket
+ * to the server.
+ *
+ * @author Philip Helger
+ */
+public final class MainStreamToServer
+{
+  public static void main (final String [] args) throws UnknownHostException, IOException
+  {
+    final File aFile = new File ("src/test/resources/test-messages/issue12-1.txt");
+    final byte [] aPayload = StreamHelper.getAllBytes (FileHelper.getInputStream (aFile));
+
+    final Socket aSocket = new Socket ("localhost", 10080);
+    try
+    {
+      final OutputStream out = aSocket.getOutputStream ();
+      out.write (aPayload);
+      out.close ();
+      System.out.println ("Stream payload to AS2 server");
+    }
+    finally
+    {
+      aSocket.close ();
+    }
+  }
+}
